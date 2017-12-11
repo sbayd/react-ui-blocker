3e045cf4d42d403b7792e517a1b8ff5564fcceaf
Berkay Aydin <sbayd06@gmail.com>
2017-12-11 22:42:01 +0300

    Update build files and build method

---
+++ build/loadingTypes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var loadingTypes = exports.loadingTypes = {
+  dot: 'sk-dot|>sk-dot1|sk-dot2',
+  rect: 'sk-rect|>rect1|rect2|rect3|rect4|rect5',
+  cube: 'sk-cube|>sk-cube1|sk-cube2',
+  bounce: 'sk-bounce|>bounce1|bounce2|bounce3',
+  circle: 'sk-circle|>sk-circle1 sk-child|sk-circle2 sk-child|sk-circle3 sk-child|sk-circle4 sk-child|sk-circle5 sk-child|sk-circle6 sk-child|sk-circle7 sk-child|sk-circle8 sk-child|sk-circle9 sk-child|sk-circle10 sk-child|sk-circle11 sk-child|sk-circle12 sk-child',
+  cubeGrid: 'sk-cube-grid|>sk-cube-child sk-cube-grid1|sk-cube-child sk-cube-grid2|sk-cube-child sk-cube-grid3|sk-cube-child sk-cube-grid4|sk-cube-child sk-cube-grid5|sk-cube-child sk-cube-grid6|sk-cube-child sk-cube-grid7|sk-cube-child sk-cube-grid8|sk-cube-child sk-cube-grid9',
+  foldingCube: 'sk-folding-cube|>sk-cubechild1 sk-cube-parent|sk-cubechild2 sk-cube-parent|sk-cubechild3 sk-cube-parent',
+  fadingCircle: 'sk-fading-circle|>sk-fading-circle1 sk-circle-child|sk-fading-circle2 sk-circle-child|sk-fading-circle3 sk-circle-child|sk-fading-circle4 sk-circle-child|sk-fading-circle5 sk-circle-child|sk-fading-circle6 sk-circle-child\sk-fading-circle7 sk-circle-child|sk-fading-circle8 sk-circle-child|sk-fading-circle9 sk-circle-child|sk-fading-circle10 sk-circle-child|sk-fading-circle11 sk-circle-child|sk-fading-circle12 sk-circle-child'
+};
+
+exports.default = loadingTypes;
